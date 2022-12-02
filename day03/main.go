1a44addd54e36a139049cc583357a8453fa4ffe5
Mike Helmick <helmick@google.com>
2022-12-02 12:40:17 -0800

    starter for day03

---
+++ day03/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		log.Printf("line: %v\n", line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+}
